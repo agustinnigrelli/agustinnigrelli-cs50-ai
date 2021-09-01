c00a2b01b9dac6fa66c96faa891b3556a40b6938
unknown <agustinnigrelli@gmail.com>
2021-09-01 20:43:23 -0300

    Solved

--- shopping.py
+++ shopping.py
@@ -1,8 +1,10 @@
 import csv
 import sys
+import calendar
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 
 TEST_SIZE = 0.4
 
@@ -35,7 +37,6 @@ def load_data(filename):
     """
     Load shopping data from a CSV file `filename` and convert into a list of
     evidence lists and a list of labels. Return a tuple (evidence, labels).
-
     evidence should be a list of lists, where each list contains the
     following values, in order:
         - Administrative, an integer
@@ -55,11 +56,46 @@ def load_data(filename):
         - TrafficType, an integer
         - VisitorType, an integer 0 (not returning) or 1 (returning)
         - Weekend, an integer 0 (if false) or 1 (if true)
-
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    month_index = dict()
+    for index, month in enumerate(calendar.month_abbr):
+        if month == 'Jun':
+            month = 'June'
+        
+        month_index[month] = index-1
+
+    evidence = []
+    labels = []
+
+    with open(filename, 'r') as data:
+        reader = csv.DictReader(data)
+        
+        for row in reader:
+            evidence.append([
+                int(row['Administrative']),
+                int(row['OperatingSystems']),
+                int(row['Browser']),
+                int(row['Region']),
+                int(row['TrafficType']),
+                int(row['Informational']),
+                int(row['ProductRelated']),
+                float(row['Administrative_Duration']),
+                float(row['Informational_Duration']),                
+                float(row['ProductRelated_Duration']),
+                float(row['BounceRates']),
+                float(row['ExitRates']),
+                float(row['PageValues']),
+                float(row['SpecialDay']),
+                month_index[row['Month']],
+                1 if row['VisitorType'] == 'Returning_Visitor' else 0,
+                1 if row['Weekend'] == 'TRUE' else 0
+            ])
+
+            labels.append(1 if row['Revenue'] == 'TRUE' else 0)
+
+    return (evidence, labels)
 
 
 def train_model(evidence, labels):
@@ -67,26 +103,33 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    return KNeighborsClassifier(n_neighbors=1).fit(evidence, labels)
 
 
 def evaluate(labels, predictions):
     """
     Given a list of actual labels and a list of predicted labels,
-    return a tuple (sensitivity, specificty).
-
+    return a tuple (sensitivity, specificity).
     Assume each label is either a 1 (positive) or 0 (negative).
-
     `sensitivity` should be a floating-point value from 0 to 1
     representing the "true positive rate": the proportion of
     actual positive labels that were accurately identified.
-
     `specificity` should be a floating-point value from 0 to 1
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
-
+    matrix = confusion_matrix(labels, predictions)
+    
+    true_pos = matrix[0][0]
+    false_pos = matrix[0][1]
+    false_neg = matrix[1][0]
+    true_neg = matrix[1][1]
+    
+    sensitivity = true_pos / (true_pos + false_neg)
+    specificity = true_neg / (true_neg + false_pos)
+    
+    return (sensitivity, specificity)
+    
 
 if __name__ == "__main__":
     main()
